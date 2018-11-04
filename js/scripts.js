7ea3ac06070b5b83058bb20bd1bd3d0f4e8fac5c
Jose Lorenzo <jolorenzom@gmail.com>
2018-11-04 20:34:55 +0100

    Comenzando a maquetar

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,40 @@ $(document).ready(function(){
         $(".nav").find(".active").removeClass("active");
         $(this).parent().addClass("active");
     });
+
+    $("#servicios-link").click(function () {
+        console.log("Click en servicios-link");
+        $("#servicios").removeClass("invisibility");
+        $("#instalaciones").addClass("invisibility");
+        $("#registro").addClass("invisibility");
+        $("#login").addClass("invisibility");
+        $("#home").addClass("invisibility");
+    });
+
+    $("#instalaciones-link").click(function () {
+        console.log("Click en instalaciones-link");
+        $("#instalaciones").removeClass("invisibility");
+        $("#servicios").addClass("invisibility");
+        $("#registro").addClass("invisibility");
+        $("#login").addClass("invisibility");
+        $("#home").addClass("invisibility");
+    });
+
+    $("#registro-link").click(function () {
+        console.log("Click en registro-link");
+        $("#registro").removeClass("invisibility");
+        $("#instalaciones").addClass("invisibility");
+        $("#servicios").addClass("invisibility");
+        $("#login").addClass("invisibility");
+        $("#home").addClass("invisibility");
+    });
+
+    $("#login-link").click(function () {
+        console.log("Click en login-link");
+        $("#login").removeClass("invisibility");
+        $("#instalaciones").addClass("invisibility");
+        $("#registro").addClass("invisibility");
+        $("#home").addClass("invisibility");
+    });
 });
 
