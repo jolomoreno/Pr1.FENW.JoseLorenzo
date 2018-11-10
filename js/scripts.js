710869bff75a861bb84e6129f265442d336c2129
Jose Lorenzo <jolorenzom@gmail.com>
2018-11-10 18:08:19 +0100

    Set cookie tokenAPI(desde file:/// no funciona, probarlo con el server de webstorm)

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
+function getCookie(name) {
+    const v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return v ? v[2] : null;
+}
+
 $(document).ready(function(){
-    window.document.cookie = "PRUEBA=PRUEBA; path=/";
-    console.log(window.document.cookie);
+    const result = getCookie('tokenAPI');
+
+    console.log(result , '<<<< RESULT');
+
+    if (result){
+        $('#login-li').addClass('invisibility');
+    }else{
+        $('#logout-li').addClass('invisibility');
+        $('#reservar-li').addClass('invisibility');
+    }
+
 
     $(".nav a").on("click", function(){
         $(".nav").find(".active").removeClass("active");
@@ -15,6 +29,7 @@ $(document).ready(function(){
         $("#home").addClass("invisibility");
         $("#emptyUser").addClass("invisibility");
         $("#emptyPassword").addClass("invisibility");
+        $("#wrongUserPassword").addClass("invisibility");
     });
 
     $("#instalaciones-link").click(function () {
@@ -25,6 +40,7 @@ $(document).ready(function(){
         $("#home").addClass("invisibility");
         $("#emptyUser").addClass("invisibility");
         $("#emptyPassword").addClass("invisibility");
+        $("#wrongUserPassword").addClass("invisibility");
     });
 
     $("#registro-link").click(function () {
@@ -35,6 +51,7 @@ $(document).ready(function(){
         $("#home").addClass("invisibility");
         $("#emptyUser").addClass("invisibility");
         $("#emptyPassword").addClass("invisibility");
+        $("#wrongUserPassword").addClass("invisibility");
     });
 
     $("#login-link").click(function () {
@@ -45,6 +62,7 @@ $(document).ready(function(){
         $("#servicios").addClass("invisibility");
         $("#emptyUser").addClass("invisibility");
         $("#emptyPassword").addClass("invisibility");
+        $("#wrongUserPassword").addClass("invisibility");
     });
 
     $("#btnLogin").click(function () {
@@ -56,32 +74,40 @@ $(document).ready(function(){
             $("#emptyUser").addClass("invisibility");
         }
 
-        if(inputPassword === ""){
-            $("#emptyPassword").removeClass("invisibility");
+        if(inputPassword === ''){
+            $('#emptyPassword').removeClass('invisibility');
         }else{
-            $("#emptyPassword").addClass("invisibility");
+            $('#emptyPassword').addClass('invisibility');
         }
 
-        console.log("User: " + inputUser);
-        console.log("Password: " + inputPassword);
+        console.log('User: ' + inputUser);
+        console.log('Password: ' + inputPassword);
 
         $.ajax({
-            url: "http://fenw.etsisi.upm.es:5555/users/login?username="+inputUser+"&password="+inputPassword,
+            url: 'http://fenw.etsisi.upm.es:5555/users/login?username='+inputUser+'&password='+inputPassword,
             async: true,
-            type: "GET",
+            type: 'GET',
             success: function (data, texStatus, request) {
-                console.log("DATA: "+ data);
-                console.log("STATUS: "+ texStatus);
-                console.log("Authorization: "+ request.getResponseHeader("Authorization"));
-                window.document.cookie = "tokenAPI="+request.getResponseHeader("Authorization")+"; path=/";
-                console.log(window.document.cookie);
+                console.log('DATA: '+ data);
+                console.log('STATUS: '+ texStatus);
+                console.log('Authorization: '+ request.getResponseHeader('Authorization'));
+                window.document.cookie = 'tokenAPI='+request.getResponseHeader('Authorization')+'; path=/ ;expires= 01 Dec 2020 00:00:01 GMT;';
+                $('#login-li').addClass("invisibility");
+                window.document.location.href = 'index.html';
             },
             error: function (e) {
-                console.log("ERROR: " + e);
+                console.log('ERROR: ' + e);
+                $('#wrongUserPassword').removeClass("invisibility");
+
             }
         });
-
-
     });
 
+    $('#logout-link').click(function () {
+        console.log(window.document.cookie);
+        window.document.cookie = 'tokenAPI=; path=/ ;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        window.document.location.href = 'index.html';
+        $('#logout-li').addClass('invisibility');
+        $('#reservar-li').addClass('invisibility');
+    });
 });
